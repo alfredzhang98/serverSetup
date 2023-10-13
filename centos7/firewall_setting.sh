6fed6327edbda21126425bc92c1af81ed8c4e7a9
Alfred <alfred.zhang98@gmail.com>
2023-10-13 13:15:28 +0800

    first commit

---
+++ centos7/firewall_setting.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+while true
+do
+    echo -e "\033[32m ******** \033[0m"
+    echo -e "\033[32m Please select an operation to perform: \033[0m"
+    echo -e "\033[32m 1. View firewall status and ports \033[0m"
+    echo -e "\033[32m 2. Restart firewall \033[0m"
+    echo -e "\033[32m 3. Start firewall \033[0m"
+    echo -e "\033[32m 4. Stop firewall \033[0m"
+    echo -e "\033[32m 5. Add a port to the public zone \033[0m"
+    echo -e "\033[32m 6. Add a service \033[0m"
+    echo -e "\033[32m 7. Deleted a port from the public zone \033[0m"
+    echo -e "\033[32m 8. Deleted a service \033[0m"
+    echo -e "\033[32m 0. Exit \033[0m"
+    echo -e "\033[32m ******** \033[0m"
+
+    read -p "Enter the corresponding number for the operation: " choice
+
+    case $choice in
+        1)
+            systemctl status firewalld
+            firewall-cmd --list-ports
+            ;;
+        2)
+            systemctl restart firewalld
+            ;;
+        3)
+            systemctl start firewalld
+            systemctl enable firewalld
+            ;;
+        4)
+            systemctl stop firewalld
+            systemctl disable firewalld
+            ;;
+        5)
+            read -p "Enter the port number to add: " port
+            read -p "Enter the protocol type (tcp/udp/icmp): " service
+            firewall-cmd --zone=public --add-port=$port/$service --permanent
+            firewall-cmd --reload
+            firewall-cmd --list-ports
+            ;;
+        6)
+            read -p "Enter the service name to add: " service
+            firewall-cmd --add-service=$service --permanent
+            firewall-cmd --reload
+            firewall-cmd --list-services
+            ;;
+        7)
+            read -p "Enter the port/service to remove: " portService
+            firewall-cmd --zone=public --remove-port=$portService
+            firewall-cmd --reload
+            firewall-cmd --list-ports
+            ;;
+        8)
+            read -p "Enter the service name to add: " service
+            firewall-cmd --remove-service=$service --permanent
+            firewall-cmd --reload
+            firewall-cmd --list-services
+            ;;
+        0)
+            echo "Exiting the script"
+            exit 0
+            ;;
+        *)
+            echo "Invalid selection"
+            ;;
+    esac
+done
---
+++ centos7/user_setting.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+
+# view current groups
+view_group() {
+  echo "Current groups:"
+  cat /etc/group
+}
+
+# Create a new group
+add_group() {
+  read -p "Enter new group name: " groupname
+  if [ -n "$groupname" ]; then
+    if ! getent group "$groupname" >/dev/null; then
+        groupadd $groupname
+        cat /etc/group | grep $groupname
+    else
+        echo "Group $groupname has been added."
+    fi
+  else
+    echo "Wrong input!"
+  fi
+  view_group
+}
+
+# Delete a group
+delete_group() {
+  read -p "Enter group name: " groupname
+  if [ -n "$groupname" ]; then
+  
+    if ! getent group "$groupname" >/dev/null; then
+        echo "Group $groupname is not exist."
+    else
+        groupdel "$groupname"
+        echo "Group $groupname deleted."
+    fi
+  else
+    echo "Wrong input!"
+  fi
+  view_group
+}
+
+
+# View user account info
+view_user() {
+  echo "Current users:"
+  for user in $(ls /home)
+  do
+    groups=$(groups $user | cut -d' ' -f2-)
+    echo "$user $groups"
+  done
+} 
+
+# Add a user and assign to group
+add_user(){
+  read -p "Enter username: " username
+  read -p "Enter password: " password
+  read -p "Enter group (or leave blank for default): " group
+  
+  if [ -z "$group" ]; then
+    $group="unclassified"
+  fi
+
+  if ! getent group "$group" >/dev/null; then
+    echo "You should add a right group first"
+  else
+    useradd -g "$group" -m "$username"
+    echo "$password" | passwd --stdin "$username"
+    echo "AllowUsers $username" >> /etc/ssh/sshd_config
+    systemctl restart sshd.service
+    systemctl enable sshd.service
+  fi
+  view_user
+}
+
+# Delete a user
+delete_user() {
+  read -p "Enter username: " username
+
+  if id "$username" &>/dev/null; then  
+    read -p "Confirm delete user $username? (y/n) " confirm
+    if [ "$confirm" = "y" ]; then
+      chown -R root:root /home/"$username"
+      rm -rf /home/"$username"
+      userdel "$username"
+      echo "User $username deleted."
+      sed -i "/$username/d" /etc/ssh/sshd_config
+      systemctl restart sshd.service
+      systemctl enable sshd.service
+    else
+      echo "Delete canceled."
+    fi
+  else
+    echo "User $username does not exist."
+  fi
+  view_user
+}
+
+# Change user password
+modify_user_password() {
+  read -p "Enter username: " username
+  passwd "$username"
+  echo "Password changed for $username."
+}
+
+# Modify user's group  
+modify_user_group() {
+  read -p "Enter username: " username
+  read -p "Enter new group: " newgroup
+  usermod -g $newgroup $username
+  echo "Changed group for $username to $newgroup."
+}
+
+# Modify user permissions
+modify_user_permissions() {
+  read -p "Enter username: " username
+  # Show current permissions
+  echo "Current permissions:"
+  su "$username" -c 'sudo -l'
+
+  has_sudo=$(grep -c "$username" /etc/sudoers)
+  if [ $has_sudo -eq 0 ]; then
+    read -p "$username currently has no sudo privilege. Add privilege? (y/n) " add
+    if [ "$add" = "y" ]; then
+      read -p "Enter sudo permission for $username ("ALL" is easy choice): " sudo_permission
+      echo "$username ALL=(ALL) $sudo_permission" >> /etc/sudoers
+      echo "Added sudo privilege for $username"
+    elif [ "$add" = "n" ]; then
+      echo "Sudo privilege unchanged for $username"
+    fi
+  else
+    read -p "$username currently has sudo privilege. Revoke privilege? (y/n) " revoke  
+    if [ "$revoke" = "y" ]; then
+      echo "Sudo privilege unchanged for $username"
+    elif [ "$revoke" = "n" ]; then
+      sed -i "/$username/d" /etc/sudoers
+      echo "Revoked sudo privilege for $username"
+    fi
+  fi
+}
+
+group_setting(){
+  echo -e "\033[32m 1. View group \033[0m"
+  echo -e "\033[32m 2. Add group \033[0m"
+  echo -e "\033[32m 3. Delete group \033[0m"
+  echo -e "\033[32m 0. Quit \033[0m"
+  echo -e "\033[32m ******** \033[0m"
+  read -p "Enter selection: " selection
+
+  case $selection in
+    1) view_group ;;
+    2) add_group ;;
+    3) delete_group ;;
+    0) break ;;
+    *) echo "Invalid selection";;
+  esac
+}
+
+user_setting(){
+  echo -e "\033[32m 1. View user \033[0m"
+  echo -e "\033[32m 2. Add user \033[0m"
+  echo -e "\033[32m 3. Delete user \033[0m"
+  echo -e "\033[32m 4. Modify user password \033[0m"
+  echo -e "\033[32m 5. Modify user group \033[0m"
+  echo -e "\033[32m 6. Modify user permissions \033[0m"
+  echo -e "\033[32m 0. Quit \033[0m"
+  echo -e "\033[32m ******** \033[0m"
+  read -p "Enter selection: " selection
+
+
+  case $selection in
+    1) view_user ;;
+    2) add_user ;;
+    3) delete_user ;;
+    4) modify_user_password ;;
+    5) modify_user_group ;;
+    6) modify_user_permissions ;;
+    0) break ;;
+    *) echo "Invalid selection";;
+  esac
+}
+
+
+
+
+# Main menu
+echo "****************************************************************"
+echo "Group should be make sure before setting a new user"
+echo "****************************************************************"
+
+while :
+do
+  echo -e "\033[32m ********"
+  echo -e "\033[32m 1. Group setting"
+  echo -e "\033[32m 2. User setting"
+  echo -e "\033[32m 0. Quit"
+  echo -e "\033[32m ********"
+  read -p "Enter selection: " selection
+
+  case $selection in
+    1) group_setting ;;
+    2) user_setting ;;
+    0) break ;;
+    *) echo "Invalid selection";;
+  esac
+
+#   echo "********"
+#   read -p "Confirm wheater to continue the setting? (y/n) " confirm
+#   if [ "$confirm" = "n" ]; then
+#     break
+#   fi
+done
+
+echo "Script finished."
