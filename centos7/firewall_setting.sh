a9bc6ff71daa6f1d983f36a1874df6ea50860d8d
Alfred <alfred.zhang98@gmail.com>
2023-12-14 19:16:02 +0800

    update the docker and the firewall

--- centos7/firewall_setting.sh
+++ centos7/firewall_setting.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
+enable_zone_drifting() {
+    echo "Enabling Zone Drifting in firewalld..."
+    sed -i 's/AllowZoneDrifting=no/AllowZoneDrifting=yes/' /etc/firewalld/firewalld.conf
+    systemctl restart firewalld
+    echo "Zone Drifting enabled."
+}
+
+
+disable_zone_drifting() {
+    echo "Disabling Zone Drifting in firewalld..."
+    firewall-cmd --set-log-denied=all
+    sed -i 's/AllowZoneDrifting=yes/AllowZoneDrifting=no/' /etc/firewalld/firewalld.conf
+    systemctl restart firewalld
+    echo "Zone Drifting disabled."
+}
+
+
 while true; do
     echo -e "\033[32m ******** \033[0m"
     echo -e "\033[32m Please select an operation to perform: \033[0m"
-    echo -e "\033[32m 1. View firewall status and ports \033[0m"
-    echo -e "\033[32m 2. Restart firewall \033[0m"
-    echo -e "\033[32m 3. Start firewall \033[0m"
-    echo -e "\033[32m 4. Stop firewall \033[0m"
-    echo -e "\033[32m 5. Add a port to the public zone \033[0m"
-    echo -e "\033[32m 6. Add a service \033[0m"
-    echo -e "\033[32m 7. Deleted a port from the public zone \033[0m"
-    echo -e "\033[32m 8. Deleted a service \033[0m"
+    echo -e "\033[32m 1. View firewall status \033[0m"
+    echo -e "\033[32m 2. List ports in all zones \033[0m"
+    echo -e "\033[32m 3. List all available services \033[0m"
+    echo -e "\033[32m ******** \033[0m"
+    echo -e "\033[32m 6. Restart firewall \033[0m"
+    echo -e "\033[32m 7. Start firewall \033[0m"
+    echo -e "\033[32m 8. Stop firewall \033[0m"
+    echo -e "\033[32m ******** \033[0m"
+    echo -e "\033[32m 10. Add a port to a zone \033[0m"
+    echo -e "\033[32m 11. Add a service \033[0m"
+    echo -e "\033[32m 12. Delete a port from a zone \033[0m"
+    echo -e "\033[32m 13. Delete a service \033[0m"
+    echo -e "\033[32m ******** \033[0m"
+    echo -e "\033[32m 20. Ensable zone drifting \033[0m"
+    echo -e "\033[32m 21. Disable zone drifting \033[0m"
+    echo -e "\033[32m ******** \033[0m"
     echo -e "\033[32m 0. Exit \033[0m"
     echo -e "\033[32m ******** \033[0m"
 
@@ -19,44 +44,64 @@ while true; do
     case $choice in
     1)
         systemctl status firewalld
-        firewall-cmd --list-ports
         ;;
     2)
-        systemctl restart firewalld
+        echo "Listing ports for all zones:"
+        for zone in $(firewall-cmd --get-zones); do
+            echo "Zone: $zone"
+            firewall-cmd --zone=$zone --list-ports
+        done
         ;;
     3)
+        echo "Listing all available services:"
+        firewall-cmd --get-services
+        ;;
+    6)
+        systemctl restart firewalld
+        ;;
+    7)
         systemctl start firewalld
         systemctl enable firewalld
         ;;
-    4)
+    8)
         systemctl stop firewalld
         systemctl disable firewalld
         ;;
-    5)
-        read -p "Enter the port number to add: " port
-        read -p "Enter the protocol type (tcp/udp/icmp): " service
-        firewall-cmd --zone=public --add-port=$port/$service --permanent
+    10)
+        read -p "Enter the port number to add (e.g., 8080): " port
+        read -p "Enter the protocol type (tcp/udp/icmp): " protocol
+        echo "Available zones:"
+        firewall-cmd --get-zones
+        read -p "Enter the zone to which you want to add the port (e.g., public, docker): " zone
+        firewall-cmd --zone=$zone --add-port=$port/$protocol --permanent
         firewall-cmd --reload
-        firewall-cmd --list-ports
+        echo "Ports in the $zone zone:"
+        firewall-cmd --zone=$zone --list-ports
         ;;
-    6)
-        read -p "Enter the service name to add: " service
+    11)
+        read -p "Enter the service name to add (e.g., http): " service
         firewall-cmd --add-service=$service --permanent
         firewall-cmd --reload
         firewall-cmd --list-services
         ;;
-    7)
-        read -p "Enter the port/service to remove: " portService
-        firewall-cmd --zone=public --remove-port=$portService
+    12)
+        read -p "Enter the port number to remove (e.g., 8080): " port
+        read -p "Enter the protocol type (tcp/udp/icmp): " protocol
+        echo "Available zones:"
+        firewall-cmd --get-zones
+        read -p "Enter the zone to which you want to remove the port (e.g., public, docker): " zone
+        firewall-cmd --zone=$zone --remove-port=$port/$protocol --permanent
         firewall-cmd --reload
-        firewall-cmd --list-ports
+        firewall-cmd --zone=$zone --list-ports
         ;;
-    8)
-        read -p "Enter the service name to add: " service
+    13)
+        read -p "Enter the service name to delete (e.g., http): " service
         firewall-cmd --remove-service=$service --permanent
         firewall-cmd --reload
         firewall-cmd --list-services
         ;;
+    20) enable_zone_drifting ;;
+    21) disable_zone_drifting ;;
     0)
         echo "Exiting the script"
         exit 0
