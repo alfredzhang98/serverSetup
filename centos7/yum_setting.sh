300bc41ff4d9ada1ab0ead007015a23047a42152
Alfred Zhang <52687013+alfredzhang98@users.noreply.github.com>
2024-01-18 11:11:27 +0800

    Update yum_setting.sh

--- centos7/yum_setting.sh
+++ centos7/yum_setting.sh
@@ -5,12 +5,13 @@ main_menu() {
 
         echo -e "\033[32m ******** \033[0m"
         echo -e "\033[32m Please select an operation to perform: \033[0m"
-        echo -e "\033[32m 1. init yum (once is ok) \033[0m"
-        echo -e "\033[32m 2. install Baota panel\033[0m"
-        echo -e "\033[32m 3. install Baota safety monitoring\033[0m"
-        echo -e "\033[32m 4. install Baota WAF\033[0m"
-        echo -e "\033[32m 5. install Baota log analysis\033[0m"
-        echo -e "\033[32m 6. install Baota security system\033[0m"
+        echo -e "\033[32m 1. update yum\033[0m"
+        echo -e "\033[32m 2. initial install yum packages (once is ok)\033[0m"
+        echo -e "\033[32m 3. install Baota panel\033[0m"
+        echo -e "\033[32m 4. install Baota safety monitoring\033[0m"
+        echo -e "\033[32m 5. install Baota WAF\033[0m"
+        echo -e "\033[32m 6. install Baota log analysis\033[0m"
+        echo -e "\033[32m 7. install Baota security system\033[0m"
         echo -e "\033[32m 0. Exit \033[0m"
         echo -e "\033[32m ******** \033[0m"
 
@@ -21,7 +22,9 @@ main_menu() {
             # 系统更新
             echo "Updating system packages..."
             yum -y update
-
+            echo "Success updating system packages"
+            ;;
+        2)
             # 自动更新工具安装与配置
             echo "Installing and configuring yum-cron for automatic updates..."
             yum -y install yum-cron
@@ -51,21 +54,21 @@ main_menu() {
 
             echo "Initial setup completed."
             ;;
-        2)
+        3)
             yum install -y wget && wget -O install.sh https://download.bt.cn/install/install_6.0.sh && sh install.sh ed8484bec
             ;;
-        3)
+        4)
             if [ -f /usr/bin/curl ]; then curl -sSO https://download.bt.cn/install/install_btmonitor.sh; else wget -O install_btmonitor.sh https://download.bt.cn/install/install_btmonitor.sh; fi
             bash install_btmonitor.sh
             ;;
-        4)
+        5)
             URL=https://download.bt.cn/cloudwaf/scripts/install_cloudwaf.sh && if [ -f /usr/bin/curl ]; then curl -sSO "$URL"; else wget -O install_cloudwaf.sh "$URL"; fi
             bash install_cloudwaf.sh
             ;;
-        5)
+        6)
             curl -sSO http://download.bt.cn/btlogs/btlogs.sh && bash btlogs.sh install
             ;;
-        6) 
+        7) 
             URL=https://download.bt.cn/bthids/scripts/install_hids.sh && if [ -f /usr/bin/curl ];then curl -sSO "$URL" ;else wget -O install_hids.sh "$URL"; fi
             bash install_hids.sh
             ;;
